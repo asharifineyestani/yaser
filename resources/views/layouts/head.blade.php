01e7be892dd0b5024ada6ae84e19e65296230fb6
Ali SharifiNeyestani <asharifineyestani@gmail.com>
2022-08-13 00:53:13 +0430

    add master_page

---
+++ resources/views/layouts/head.blade.php
@@ -0,0 +1,12 @@
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<meta name="author" content="Kourosh&Ali">
+<link rel="stylesheet" type="text/css" href="/fonts/fontiran.css">
+<link rel="stylesheet" type="text/css" href="/assets/css/style-rtl.css">
+<style>
+    body, h1, h2, h3, h4, h5, h6, input, textarea {
+        font-family: iranyekan !important;
+    }
+</style>
+
+
---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+
+<html lang="ar" dir="rtl">
+<head>
+    <title>به عشق یاسر</title>
+    @include('layouts.head')
+</head>
+
+
+<body lang="fa" dir="rtl">
+
+<x-navbar />
+
+<main>
+    @yield('main')
+</main>
+
+<x-footer/>
+
+
+</body>
+</html>
